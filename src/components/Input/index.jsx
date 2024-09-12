5fb8410a64e5a1b6473f40b99d86adff64733a6e
emachipana <emachipana0102@gmail.com>
2024-09-12 18:23:58 -0500

    ADD: input component

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,57 @@
+/** @jsxImportSource @emotion/react */
+import { useState } from "react";
+import { Container, Label, Main, Section, TextError } from "./styles";
+import { COLORS } from "../../styles/colors";
+
+export const onBlur = (e, setFocused, handleBlur) => {
+  setFocused(false);
+
+  handleBlur && handleBlur(e);
+}
+
+export const setColor = (error, touched, focused) => {
+  return error && touched
+  ? COLORS.red
+  : (touched && !error
+    ? COLORS.persian
+    : (focused
+        ? COLORS.persian
+        : COLORS.taupe));
+}
+
+function Input({ 
+  id, disabled, label, placeholder,
+  type, value, handleChange, handleBlur,
+  error, touched, Icon, backgroundColor, ...props }) {
+
+  const [focused, setFocused] = useState(false);
+  const color = setColor(error, touched, focused);
+
+  return (
+    <Container>
+      { label && <Label htmlFor={id}>{ label }</Label> }
+      <Section
+        isFile={type === "file"}
+        color={color}
+        backgroundColor={backgroundColor}
+      >
+        { Icon && <Icon size={25} color={color} /> }
+        <input 
+          id={id}
+          disabled={disabled}
+          placeholder={placeholder}
+          type={type || "text"}
+          value={value}
+          onChange={handleChange}
+          onBlur={(e) => onBlur(e, setFocused, handleBlur)}
+          onFocus={() => setFocused(true)}
+          css={Main}
+          {...props}
+        />
+      </Section>
+      { touched && error && <TextError>{ error }</TextError> }
+    </Container>
+  );
+}
+
+export default Input;
---
+++ src/components/Input/styles.js
@@ -0,0 +1,61 @@
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+import { FONTS } from "../../styles/fonts";
+import { COLORS } from "../../styles/colors";
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 0.5rem;
+  position: relative;
+`;
+
+export const Label = styled.label`
+  font-size: ${({ size }) => size || 18}px;
+  font-weight: 700;
+  text-align: start;
+`;
+
+export const Section = styled.section`
+  width: 100%;
+  height: ${({ isFile }) => isFile ? "auto" : "40px"};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  padding: ${({ isFile }) => isFile ? "0" : "0.3rem 0.7rem"};
+  outline: 1px solid  ${({ color }) => color};
+  outline: ${({ isFile }) => isFile ? "none" : ""};
+  border-radius: 0.5rem;
+  background-color: ${({ backgroundColor }) => backgroundColor || "transparent"};
+`;
+
+export const Main = css`
+  width: 100%;
+  height: 100%;
+  font-size: 1rem;
+  font-weight: 600;
+  border: none;
+  outline: none;
+  color: ${COLORS.gray};
+  background-color: transparent;
+  font-family: ${FONTS.primary};
+
+  &::placeholder {
+    color: ${COLORS.taupe};
+  }
+`;
+
+export const TextError = styled.p`
+  font-size: 14px;
+  padding: 1px 4px;
+  font-weight: 600;
+  color: ${COLORS.white};
+  border-radius: 4px;
+  margin-bottom: -8px;
+  background-color: ${COLORS.red};
+  opacity: .8;
+`;
