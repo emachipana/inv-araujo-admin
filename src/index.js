8899daf3a1e01ca4557128a7a69bdb94d1643d29
emachipana <emachipana0102@gmail.com>
2024-09-12 10:33:00 -0500

    ADD: global styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Global } from '@emotion/react';
+import { RESET } from './styles/reset';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Global styles={RESET} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
