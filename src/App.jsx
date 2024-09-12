a63f042f78e682f7191f1049cd7d000c85c605ce
emachipana <emachipana0102@gmail.com>
2024-09-12 18:23:25 -0500

    ADD: authentication context

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import { useAuth } from "./context/auth";
+import ClientApp from "./ClientApp";
+import AdminApp from "./AdminApp";
+
 function App() {
+  const { isLoading } = useAuth();
+
   return (
-    <h1>Hello world</h1>
+    isLoading
+    ? "Cargando..."
+    :
+    <Routes>
+      <Route index path="/*" element={<ClientApp />} />
+      <Route path="admin/*" element={<AdminApp />} />
+    </Routes>
   );
 }
 
