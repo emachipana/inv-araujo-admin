5372349a4158e16f0c820eee5e8d24892700c87f
emachipana <emachipana0102@gmail.com>
2024-09-13 11:22:32 -0500

    ADD: use components for admin panel

--- src/AdminApp/AuthenticatedApp.jsx
+++ src/AdminApp/AuthenticatedApp.jsx
@@ -1,6 +1,27 @@
+import { useState } from "react";
+import AdminNavbar from "../components/AdminNavbar";
+import { Section } from "../styles/layout";
+import { Route, Routes } from "react-router-dom";
+import Aside from "../components/Aside";
+
 function AuthenticatedApp() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <h1>Hello from authenticated app</h1>
+    <>
+      <AdminNavbar 
+        setIsOpen={setIsOpen}
+      />
+      <Aside 
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+      />
+      <Section>
+        <Routes>
+          <Route index exact path="/" element={<h1>Home page</h1>} />
+        </Routes>
+      </Section>
+    </>
   );
 }
 
