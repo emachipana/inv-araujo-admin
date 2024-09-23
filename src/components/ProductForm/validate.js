37711439e36c9dc3f652e34075ec09cda2c9e7ef
emachipana <emachipana0102@gmail.com>
2024-09-23 11:40:52 -0500

    ADD: product form component

---
+++ src/components/ProductForm/validate.js
@@ -0,0 +1,47 @@
+export const validate = (values) => {
+  const errors = {};
+
+  if(!values.name) {
+    errors.name = "Este campo es obligatorio";
+  }else if(values.name.length < 3) {
+    errors.name = "El mínimo son 3 caracteres";
+  }
+
+  if(!values.description) {
+    errors.description = "Este campo es obligatorio";
+  }else if(values.description.length < 10) {
+    errors.description = "El mínimo son 10 caracteres";
+  }
+
+  if(!values.brand) {
+    errors.brand = "Este campo es obligatorio";
+  }else if(values.brand.length < 3) {
+    errors.brand = "El mínimo son 3 caracteres";
+  }
+
+  if(!values.categoryId) errors.categoryId = "Este campo es obligatorio";
+
+  if(!values.brand) {
+    errors.brand = "Este campo es obligatorio";
+  }else if(values.brand.length < 3) {
+    errors.brand = "El mínimo son 3 caracteres";
+  }
+
+  if(!values.price) {
+    errors.price = "Este campo es obligatorio";
+  }else if(isNaN(values.price * 1)) {
+    errors.price = "Solo se aceptan números";
+  }else if(values.price <= 0) {
+    errors.price = "Solo se aceptan valores mayores a 0";
+  }
+
+  if(!values.stock) {
+    errors.stock = "Este campo es obligatorio";
+  }else if(isNaN(values.stock * 1)) {
+    errors.stock = "Solo se aceptan números";
+  }else if(values.stock <= 0) {
+    errors.stock = "Solo se aceptan valores mayores a 0";
+  }
+
+  return errors;
+}
