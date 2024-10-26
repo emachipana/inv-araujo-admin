88a3e674332c90926f8e53a80f466bdcfe113f8a
emachipana <emachipana0102@gmail.com>
2024-10-25 23:48:24 -0500

    FEAT: add search invoices

--- src/pages/admin/Invoices/Type.jsx
+++ src/pages/admin/Invoices/Type.jsx
@@ -1,28 +1,30 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useAdmin } from "../../../context/admin";
 import apiFetch from "../../../services/apiFetch";
 import { Container } from "../../../components/Categories/styles";
 import { Spinner } from "reactstrap";
 import Category from "../../../components/Category";
 
-function Type() {
-  const [currentType, setCurrentType] = useState("Todo");
+function Type({ currentType, setCurrentType, isBlocked, setIsGetting }) {
   const { isLoading, invoicesBackup, setInvoices, setError } = useAdmin();
 
   useEffect(() => {
     const fetch = async () => {
       try {
         if(currentType === "Todo") return setInvoices(invoicesBackup);
+        setIsGetting(true);
         const invoices = await apiFetch(`invoices?type=${currentType.toUpperCase()}`);
         setInvoices(invoices);
+        setIsGetting(false);
       }catch(error) {
+        setIsGetting(false);
         console.error(error);
         setError(error.message);
       }
     }
 
     fetch();
-  }, [ currentType, invoicesBackup, setError, setInvoices ]);
+  }, [ currentType, invoicesBackup, setError, setInvoices, setIsGetting ]);
 
   return (
     <Container isLoading={isLoading}>
@@ -30,18 +32,21 @@ function Type() {
         isLoading
         ? <Spinner color="secondary" />
         : <>
-            <Category 
+            <Category
+              isBlocked={isBlocked}
               name="Todo"
               currentCategory={currentType}
               setCurrentCategory={setCurrentType}
             />
-            <Category 
+            <Category
+              isBlocked={isBlocked}
               name="Factura"
               currentCategory={currentType}
               setCurrentCategory={setCurrentType}
             />
             <Category 
               name="Boleta"
+              isBlocked={isBlocked}
               currentCategory={currentType}
               setCurrentCategory={setCurrentType}
             />
